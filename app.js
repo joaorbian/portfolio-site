a31d34f7b36e10a564406930a84ec4ee356f2152
progjoao <joaoricardobianchini@hotmail.com>
2023-01-28 21:18:22 -0300

    Remove CMS functions

--- app.js
+++ app.js
@@ -1,58 +1,7 @@
 // const URL = 'http://localhost:3000'
 const URL = './db.json'
 
-async function access() {
-   const { value: password } = await Swal.fire({
-      title: 'Enter your password',
-      input: 'password',
-      inputLabel: 'Password',
-      inputPlaceholder: 'Enter your password',
-      inputAttributes: {
-         maxlength: 10,
-         autocapitalize: 'off',
-         autocorrect: 'off'
-      }
-   })
 
-   if(password === 'picapau123') {
-      // Swal.fire(`Entered password: ${password}`)
-      document.querySelector('.container').style.display = 'none'
-      document.querySelector('.register').style.display = 'block'
-   } else {
-      Swal.fire(`Você não tem acesso, não tente novamente`)
-   }
-}
-
-async function registerProject() {
-
-   const title = document.querySelector('.title').value
-   const description = document.querySelector('.description').value
-   const website = document.querySelector('.website').value
-   const github = document.querySelector('.github').value
-
-   console.log(title, description, website, github)
-
-   await fetch(`${URL}/projects`, {
-      method: 'POST',
-      headers: {
-         'Content-Type': 'application/json',
-      },
-      body: JSON.stringify({
-         created_at: new Date().toISOString().slice(0, 10),
-         updated_at: new Date().toISOString().slice(0, 10),
-         title: title,
-         description: description, 
-         website: website,
-         git: github,
-      })
-   }).then(() => Swal.fire({
-      icon: 'success',
-      title: 'Projeto registrado com sucesso',
-      showConfirmButton: false,
-      timer: 1500
-   }).then(() => location.reload(true)))
-
-}
 
 async function getData() {
    try {
@@ -88,11 +37,11 @@ function watchColorPicker(event) {
    localStorage.setItem('color', event.target.value)
    location.reload(true)
 }
+
 const color = localStorage.getItem('color')
 document.querySelector('.input-color').value = color
 document.documentElement.style.setProperty('--tertiary', color) 
 
 document.getElementById('model').style.display = 'none'
-document.querySelector('.send').addEventListener('click', registerProject)
-document.querySelector('.profession').addEventListener('click', access)
-getData()
+
+window.onload = getData()
